c2a54f5cb6ffcd8a30500ff1cf52f52706185f41
amikelevich <a.mikelevich@student.pk.edu.pl>
2026-06-22 22:53:05 +0200

    feat: dodanie wyliczania optymalnego okna ładowania

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,14 @@ const CHART_COLORS = [
 
 function App() {
   const [energyData, setEnergyData] = useState<EnergyDay[]>([]);
+  const [hours, setHours] = useState("");
+  const [optimalWindow, setOptimalWindow] = useState<{
+    from: string;
+    to: string;
+    averageCleanEnergy: number;
+  } | null>(null);
+  const [windowError, setWindowError] = useState("");
+
   useEffect(() => {
     const fetchEnergyMix = async () => {
       try {
@@ -48,6 +56,27 @@ function App() {
     };
     fetchEnergyMix();
   }, []);
+
+  const handleFindWindow = async () => {
+    const hrs = Number(hours);
+    if (hrs < 1 || hrs > 6) {
+      setWindowError("Proszę podać wartość od 1 do 6 godzin.");
+      setOptimalWindow(null);
+      return;
+    }
+    setWindowError("");
+    setOptimalWindow(null);
+
+    try {
+      const response = await axios.get(
+        `https://staz-codibly-backend.onrender.com/optimal-window/${hrs}`,
+      );
+      setOptimalWindow(response.data);
+    } catch (error) {
+      setWindowError("Spróbuj ponownie");
+    }
+  };
+
   return (
     <>
       <section>
@@ -90,6 +119,62 @@ function App() {
             </div>
           ))}
         </div>
+      </section>
+      <section
+        className="card"
+        style={{
+          borderTopWidth: "1px",
+          borderTopStyle: "solid",
+          borderColor: "white",
+          marginTop: "50px",
+        }}
+      >
+        <h2 style={{ marginTop: "10px" }}>Optymalne okno ładowania</h2>
+        <p>Podaj liczbę godzin (1-6h)</p>
+        <div className="input-group">
+          <input
+            type="number"
+            min="1"
+            max="6"
+            placeholder="np. 1"
+            value={hours}
+            onChange={(e) => setHours(e.target.value)}
+            className="custom-input"
+          />
+          <button className="custom-btn" onClick={handleFindWindow}>
+            Submit
+          </button>
+        </div>
+        {windowError && (
+          <p style={{ color: "red", marginTop: "16px", fontWeight: "bold" }}>
+            {windowError}
+          </p>
+        )}
+        {optimalWindow && (
+          <div
+            style={{
+              marginTop: "24px",
+              padding: "20px",
+              borderRadius: "12px",
+            }}
+          >
+            <h3> Najlepszy czas na łądowanie: </h3>
+            <div>
+              <strong>Start:</strong>{" "}
+              {optimalWindow.from.replace("T", " ").substring(0, 16)}
+            </div>
+            <div>
+              <strong>Koniec:</strong>
+              {optimalWindow.to.replace("T", " ").substring(0, 16)}
+            </div>
+            <div>
+              Czysta energia:{" "}
+              <strong style={{ color: "green" }}>
+                {optimalWindow.averageCleanEnergy.toFixed(2)}%
+              </strong>
+            </div>
+          </div>
+        )}
       </section>
     </>
   );
